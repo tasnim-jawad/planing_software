ce178cfc52de48926f0e79b50fe39a842f0be8e9
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2024-07-07 19:35:21 +0600

    layout complete

--- resources/js/src/MainLayout.vue
+++ resources/js/src/MainLayout.vue
@@ -5,9 +5,16 @@
                 <div class="logo_container">
                     <img class="logo_image" src="https://asset.brandfetch.io/idU6lzwMYA/idRvA5TWSD.png" alt="">
                 </div>
-                <div class="topbar_right_icon">
-                    <a href="" class="me-2 text-light"><i class="fa-solid fa-bars"></i></a>
-                    <a href="" class="text-light"><i class="fa-solid fa-ellipsis-vertical"></i></a>
+                <div class="topbar_right">
+                    <a href="" class="me-3 text-light"><i class="fa-brands fa-figma"></i></a>
+                    <div class="profile_button_container">
+                        <button class="topbar_right_profile">
+                            <div class="letter_circle">
+                                <span>TH</span>
+                            </div>
+                            <a href="" class="dropdown_icon"><i class="fa-solid fa-angle-down"></i></a>
+                        </button>
+                    </div>
                 </div>
             </div>
             <div class="layout_content">
@@ -18,35 +25,138 @@
                                 <div class="side_nave_topbar">
                                     <div class="profile_heading">
                                         <div class="profile_image_container">
-                                            <img src="https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=" alt="">
+                                            <img src="https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg="
+                                                alt="">
                                         </div>
                                         <div class="profile_name_container">
-                                            <h1 class="profile_name">Muhammad Tasnimul</h1>
-                                        </div>
-                                        <div class="seting_icon">
-                                            <a href=""><i class="fa-solid fa-angle-down"></i></a>
+                                            <div class="name">
+                                                Tasnimul Hasan
+                                            </div>
+                                            <div class="setting_icon">
+                                                <a href=""><i class="fa-solid fa-angle-down"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="sidebar_toggle">
-                                        <a href=""><i class="fa-solid fa-angle-down"></i></a>
+                                        <a href=""><i class="fa-solid fa-indent"></i></a>
                                     </div>
                                 </div>
-                                <ul>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
+                                <ul class="side_nave_lists">
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-house-user"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Home</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-address-book"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Dashboard</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-address-card"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Clips</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-folder-plus"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Profile</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                                 <div class="line_divider"></div>
-                                <ul>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
-                                    <li>dashboard</li>
+                                <ul class="side_nave_lists">
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-house-user"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Home</span>
+                                                <div class="list_right_icons">
+                                                    <a href="#"><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href="#"><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-address-book"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Dashboard</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-address-card"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Clips</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="side_nave_list_item">
+                                        <div class="list_container">
+                                            <div class="list_identity_icon">
+                                                <a href=""><i class="fa-solid fa-folder-plus"></i></a>
+                                            </div>
+                                            <div class="list_name">
+                                                <span>Profile</span>
+                                                <div class="list_right_icons">
+                                                    <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                                                    <a href=""><i class="fa-solid fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                             </div>
                         </nav>
@@ -55,15 +165,17 @@
                 <div class="layout_content_right">
                     <div class="topbar">
                         <div class="topbar_left">
-                            <a href=""><i class="fa-solid fa-angle-down"></i></a>
-
+                            <a href=""><i class="fa-solid fa-chart-column"></i></a>
+                            <span>Dashboard</span>
                         </div>
                         <div class="topbar_right">
                             <div class="topbar_right_search">
-                                <a href="search_icon"><i class="fa-solid fa-angle-down"></i></a>
-                                <input type="text">
+                                <a href="" class="search_icon"><i class="fa-solid fa-magnifying-glass"></i></a>
+                                <span>Search Dashboards</span>
                             </div>
-                            <button>New Deshboard</button>
+                            <div class="deshboard_button">
+                                <span>New Deshboard</span>
+                            </div>
                         </div>
                     </div>
                     <div class="main_content">
@@ -77,11 +189,9 @@
 </template>
 
 <script>
-    export default {
+export default {
 
-    }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
